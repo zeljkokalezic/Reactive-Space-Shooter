91d2a0a0c4268eb37dc357b7df53838322bf73db
Zeljko Kalezic <zeljkokalezic@gmail.com>
2015-08-10 19:44:40 +0200

    Todo

---
+++ Assets/SpaceShooterReactive/Scripts/GameMaster.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameMaster : MonoBehaviour {
+	//this will be a singleton that will track all the models in the game
+	//step 1: create models
+	//step 2: create events & subscriptions (on projectile hit -> destroy, on player health 0 -> game over, ...)
+	//step 3: wire the backend with the "frontend" (unity components)
+	//play :)
+}
